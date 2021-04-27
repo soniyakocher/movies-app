e9ac39b1a588cd0581bfb5d52c52a669e7ff5fda
swapnil <kocher1327@gmail.com>
2021-04-27 23:33:34 +0530

    Added Home Component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
+import Home from './screens/home/Home';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-   <div></div>
+  <Home/>
   </React.StrictMode>,
   document.getElementById('root')
 );
