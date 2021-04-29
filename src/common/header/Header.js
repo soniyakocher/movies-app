08472a644e729c837a460d09002a0baf3efea91d
swapnil <kocher1327@gmail.com>
2021-04-29 06:40:56 +0530

    Added Modal

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -2,19 +2,44 @@ import React, { Component } from 'react'
 import Button from '@material-ui/core/Button';
 import './Header.css'
 import logo from '../../assets/logo.svg';
+import Modal from 'react-modal';
 
 
 
 class Header extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            modalIsOpen: false
+             
+        }
+    }
+    
+    openModalHandler=()=>{
+        this.setState({
+            modalIsOpen:true
+        })
+    }
+
+    closeModalHandler=()=>{
+        this.setState({
+            modalIsOpen:false
+        })
+    }
     render() {
         return (
             <div>
                 <header className="app-header">
                     <img src={logo} className="app-logo" alt='logo' />
                     <div className="login-btn">
-                        <Button variant="contained" color="default" >Login</Button>
+                        <Button variant="contained" color="default"  onClick={this.openModalHandler}>Login</Button>
                     </div>
                 </header>
+                <Modal  ariaHideApp={false} isOpen={this.state.modalIsOpen} contentLabel="Login" onRequestClose={this.closeModalHandler}>
+
+                </Modal>
             </div>
         )
     }
