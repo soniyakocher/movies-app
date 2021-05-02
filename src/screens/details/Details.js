07ed4a0893f7122801d4f35e36f86a1a1717a929
swapnil <kocher1327@gmail.com>
2021-05-02 11:28:39 +0530

    Added poster, title and genres on details page

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react'
-import Header from '../../common/header/Header'
-import moviesData from '../../assets/movieData'
-
-export class Details extends Component {
-    constructor(props) {
-        super(props)
-    
+import React, { Component } from 'react';
+import Header from '../../common/header/Header';
+import moviesData from '../../common/movieData';
+import Typography from '@material-ui/core/Typography';
+import './Details.css';
+class Details extends Component {
+    constructor() {
+        super();
         this.state = {
-             movie:{}
+            movie: {}
         }
     }
-    componentDidMount() {
+    componentWillMount() {
         let currentState = this.state;
         currentState.movie = moviesData.filter((mov) => {
             return mov.id === this.props.movieId
@@ -18,19 +18,46 @@ export class Details extends Component {
         this.setState({ currentState });
         console.log(this.state);
     }
-    
     render() {
+        let movie = this.state.movie;
         return (
             <div className="details">
-                <Header/>
-                <div className='flex-container-details'>
-                    <div className='leftDetails'></div>
-                    <div className='middleDetails'></div>
-                    <div className='rightDetails'></div>
+                <Header />
+                <div className="flex-containerDetails">
+                    <div className="leftDetails">
+                        <img src={movie.poster_url} alt={movie.title} />
+                    </div>
+
+                    <div className="middleDetails">
+                        <div>
+                            <Typography variant="headline" component="h2">{movie.title} </Typography>
+                        </div>
+                        <br />
+                        <div>
+                            <Typography>
+                                <span className="bold">Genres: </span> {movie.genres.join(', ')}
+                            </Typography>
+                        </div>
+                        <div>
+                            <Typography><span className="bold">Duration:</span> {movie.duration} </Typography>
+                        </div>
+                        <div>
+                            <Typography><span className="bold">Release Date:</span> {new Date(movie.release_date).toDateString()} </Typography>
+                        </div>
+                        <div>
+                            <Typography><span className="bold"> Rating:</span> {movie.critics_rating}  </Typography>
+                        </div>
+                        <div className="marginTop16">
+                            <Typography><span className="bold">Plot:</span> <a href={movie.wiki_url}>(Wiki Link)</a> {movie.storyline} </Typography>
+                        </div>
+                    </div>
+
+                    <div className="rightDetails">
+
+                    </div>
                 </div>
             </div>
         )
     }
 }
-
-export default Details
+export default Details;
