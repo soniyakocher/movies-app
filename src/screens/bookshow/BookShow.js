43563d572d786707e90f9214dc7168ed726abea3
swapnil <kocher1327@gmail.com>
2021-05-04 11:29:32 +0530

    Added back button on book show page

--- src/screens/bookshow/BookShow.js
+++ src/screens/bookshow/BookShow.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
 import Header from '../../common/header/Header'
-
+import Home from '../home/Home'
+import ReactDOM from 'react-dom'
+import Typography from '@material-ui/core/Typography'
+import './BookShow.css'
  class BookShow extends Component {
+
+    backtohomeHandler =( ) => {
+        ReactDOM.render (<Home/>, document.getElementById('root'))
+    }
     render() {
         return (
             <div>
                 <Header/>
+                <div className="bookShow" > 
+                    <Typography className="back" onClick={this.backtohomeHandler}>
+                    &#60; Back to Home
+                    </Typography>
+                </div>
             </div>
         )
     }
